8a05c88b8fa1bb520a63cdf88333c632175448c4
Roman Vladimirov <trueromanus@gmail.com>
2023-12-31 16:37:47 +0300

    * Preparation for nativeaot

---
+++ src/SerializerContexts/TorrentStreamSerializerContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using TorrentStream.Models;
+
+namespace TorrentStream.SerializerContexts {
+
+    [JsonSerializable ( typeof ( IEnumerable<FullManagerModel> ) )]
+    [JsonSerializable ( typeof ( IEnumerable<TorrentFileModel> ) )]
+    [JsonSerializable ( typeof ( TorrentFileModel ) )]
+    [JsonSerializable ( typeof ( StatusModel ) )]
+    [JsonSerializable ( typeof ( ManagerModel ) )]
+    [JsonSerializable ( typeof ( IEnumerable<StatusModel> ) )]
+    [JsonSerializable ( typeof ( IEnumerable<ManagerModel> ) )]
+    [JsonSerializable ( typeof ( List<ManagerModel> ) )]
+    [JsonSerializable ( typeof ( string ) )]
+    [JsonSerializable ( typeof ( int ) )]
+    public partial class TorrentStreamSerializerContext : JsonSerializerContext {
+    }
+
+}
